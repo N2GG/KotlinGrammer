49b139ac59c43832d37c9d4fd05f7d9beb1eb54c
N2GG <nol2ggoon2@gmail.com>
2023-10-23 19:49:10 +0900

    Print helloworld

--- app/src/main/java/com/example/kotlingrammer/Study1.kt
+++ app/src/main/java/com/example/kotlingrammer/Study1.kt
@@ -1,10 +1,13 @@
 package com.example.kotlingrammer
 
+
+//1. 함수에 대해 알아보자
+//리턴 타입이 없을 경우엔 Unit = Void와 같은기능 은 생략 가능하다.
 fun main(){
     helloworld()
 
 }
 
-fun helloworld() {
+fun helloworld() : Unit {
     println("Hello Kotlin World")
 }
