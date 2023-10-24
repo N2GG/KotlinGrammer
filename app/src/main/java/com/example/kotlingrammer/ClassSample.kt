c7910130458c8af56bacdf92412848d164c86e0e
N2GG <nol2ggoon2@gmail.com>
2023-10-24 17:13:51 +0900

    Constructor

---
+++ app/src/main/java/com/example/kotlingrammer/ClassSample.kt
@@ -0,0 +1,33 @@
+package com.example.kotlingrammer
+
+//8. Class의 상속, 정의하는법 Constructor 이용하는법
+//class Human {
+//
+//    val name = "DaChan"
+//    fun eatingCake() {
+//        println("This is so YUMMY")
+//    }
+//}
+//fun main(){
+//    val human = Human()
+//    human.eatingCake()
+//    println("This human name is ${human.name}")
+//
+//}
+
+
+//Constructor을 이용하면 이렇게 사용한다
+
+class Human constructor(val name : String){
+
+    //val name = name
+    fun eatingCake() {
+        println("This is so YUMMY")
+    }
+}
+fun main(){
+    val human = Human("DaChan")
+    human.eatingCake()
+    println("This human name is ${human.name}")
+
+}
