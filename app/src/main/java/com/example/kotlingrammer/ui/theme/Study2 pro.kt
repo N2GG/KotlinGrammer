6ff34efbb05680076cbb515f0d2de65a81a63f7d
N2GG <nol2ggoon2@gmail.com>
2023-10-25 20:05:11 +0900

    **lamda**

--- app/src/main/java/com/example/kotlingrammer/ui/theme/Study2 pro.kt
+++ app/src/main/java/com/example/kotlingrammer/ui/theme/Study2 pro.kt
@@ -39,6 +39,24 @@ fun main() {
     println(extendString("Dachan", 31))
 
 
+    //ex.4
+    println(calculateGrade(110))
+
+
+    //ex.5
+    val lamda = {number : Double -> number == 4.234}
+
+    println(invokeLamda(lamda))
+
+    //함수 선언을 lamda로 하였지만 그걸 사용하지 않고 바로 {}로 사용할수 있는데 이를 람다 리터럴이라 한다
+    println(invokeLamda({it > 6}))
+
+    //만약 Function의 마지막 파라미터가 lamda식일 경우에 밑 처럼 ()를 생략을 할 수 있다
+    //위의 식과 같다
+    println(invokeLamda{it > 6})
+
+
+
 }
 
 //2. 확장함수
@@ -54,4 +72,27 @@ fun extendString(name : String, age : Int) : (String) {
     //밑의 introduceMyself 처럼 파라미터가 딱 하나(Int) 있을경우엔, it 을사용하여 받아올 수 있다.
     val introduceMyself : String.(Int) -> String = {"My name is ${this} and I'm ${it} years old"}
     return name.introduceMyself(age)
+}
+
+//람다의 Return
+//기본적으로 람다는 마지막 한줄이 return을 의미한다
+//ex.4 범위값을 지정해줄 때에, 이외의 값, 즉 else로 마무리 하지않으면 에러가 발생한다.
+
+//ex.4
+val calculateGrade : (Int) -> String = {
+    when(it) {
+        in 0..40 -> "fail"
+        in 41.. 70 -> "pass"
+        in 71..100 -> "perfect"
+        else -> "Error"
+    }
+}
+
+
+//람다를 표현하는 여러가지 방법
+
+//ex.5
+fun invokeLamda(lamda : (Double) -> Boolean) : Boolean {
+    return lamda(5.2466)
+
 }
